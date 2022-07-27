31409684ccc1304f9e921d84c34c93c5e0285702
Mohammad Helwa <al-gorithmi@v1040-wn-rt-b-122-82.campus-dynamic.uwaterloo.ca>
2022-07-27 16:56:23 -0400

    Added siteid to IP info.

--- web-server/front-end/src/components/card-elements/siteInformation.vue
+++ web-server/front-end/src/components/card-elements/siteInformation.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <div class="list-group" style="overflow-y: auto; height: 400px;" >
-            <button v-for="(ip, index) in ipAddresses" :key="ip" class="list-group-item" data-bs-toggle="modal" :data-bs-target="'#ip' + names[index]">{{ ip }}</button>
+            <button v-for="(ip, index) in ipAddresses" :key="ip" class="list-group-item" data-bs-toggle="modal" :data-bs-target="'#ip' + names[index]">{{ siteIds[index] + ': ' + ip }}</button>
         </div>
     </div>
 </template>
@@ -17,7 +17,9 @@ export default {
      */
     data(){
         return {
+            ipSitePair: {},
             ipAddresses: [], // List of IP addresses.
+            siteIds: [], // List of siteIds.
             names: [] // List of modal names.
         }
     },
@@ -27,13 +29,22 @@ export default {
     }, 
 
     async mounted() {
-        await fetch(`http://${Constants.PUBLIC_IP}:8001/api/v1/ipInfo/fetch`)
+        await fetch(`http://${Constants.PUBLIC_IP}:8001/api/v1/sites/fetch`)
             .then(response => response.json())
             .then((jsonResponse) => {
                 for (var i in jsonResponse) {
-                    this.ipAddresses.push(jsonResponse[i].ip);
-                    this.names.push(jsonResponse[i].ip.replace(/\./g, ''));
+                    this.ipSitePair[jsonResponse[i].current.siteId] =  jsonResponse[i].current.ip;
+                    this.siteIds.push(jsonResponse[i].current.siteId);
+                    this.names.push(jsonResponse[i].current.ip.replace(/\./g, ''));
                 }
+                this.siteIds.sort();
+                console.log(this.ipSitePair);
+                console.log(this.siteIds);
+                for (var j in this.siteIds) {
+                    console.log(this.ipSitePair[this.siteIds[j]]);
+                    this.ipAddresses.push(this.ipSitePair[this.siteIds[j]]);
+                }
+                console.log(this.ipAddresses);
             });
     }
 }
