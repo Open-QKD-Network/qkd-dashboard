31c619ca103baa9ed837f9aa614d0f62412bd8b4
mhahelwa2020 <mhahelwa@gmail.com>
2022-08-02 17:03:13 -0400

    Fixed bug

--- aws-server/api/availableNetworkApi.js
+++ aws-server/api/availableNetworkApi.js
@@ -4,12 +4,11 @@ const Connection = require("../controllers/connections");
  * Site Information API class.
  */
 module.exports = class AvailableNetworkApi {
-    constructor () {
-        this.connectionClass = new Connection;
-    }
+
     static async fetchTopologyAsync(req, res) {
         try  {
-            const available = this.connectionClass.checkIfFileExists();
+            const connectionClass = new Connection;
+            const available = connectionClass.checkIfFileExists();
             res.status(200).json({available: available});
         } catch (err) {
             res.status(404).json({message: err.message});
